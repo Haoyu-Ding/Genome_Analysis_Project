201e41b29029bc87b6a107087eefc9257df5af15
Haoyu Ding <dinghy@pelle1.uppmax.uu.se>
2026-04-16 00:55:05 +0200

    restructure directory and add one script for Pacbio data qc and preprocessing

---
+++ code/script/01_pacbio_qc_and_preprocess.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2026-1-61
+#SBATCH -p pelle
+#SBATCH -n 2
+#SBATCH -t 03:00:00
+#SBATCH -J pacbio_qc_and_preprocess
+#SBATCH --mail-type=ALL
+#SBATCH --output=%x.%j.out
+
+# Load modules
+module load bioinfo-tools
+module load FastQC/0.12.1-Java-17
+module load NanoFilt/2.8.0-foss-2023b
+
+cd /home/dinghy/Genome_Analysis_Project
+
+# 1.quality control(before)
+mkdir -p analysis/output/fastqc_before
+fastqc data/genomics/pacbio/raw/*.subreads.fastq.gz \
+    --outdir analysis/output/fastqc_before \
+    --threads 2
+
+# 2.preprocess
+for f in data/genomics/pacbio/raw/*.subreads.fastq.gz; do
+    base=$(basename $f .subreads.fastq.gz)
+    zcat $f | NanoFilt -l 500 -q 7 | gzip > data/genomics/pacbio/filtered/${base}.filtered.fastq.gz
+done
+
+# combine them into one
+zcat data/genomics/pacbio/filtered/*.filtered.fastq.gz | gzip > data/genomics/pacbio/filtered/all_filtered.fastq.gz
+
+# 3.quality control(after)
+mkdir -p analysis/output/fastqc_after
+fastqc data/genomics/pacbio/filtered/all_filtered.fastq.gz \
+    --outdir analysis/output/fastqc_after \
+    --threads 2
