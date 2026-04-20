9c1e6dbe67512a4315a8436ba74c0b8d06b7c1cc
Haoyu Ding <dinghy@pelle1.uppmax.uu.se>
2026-04-21 00:17:21 +0200

    delete old qc and trim script, add canu script for pacbio assembly

--- code/script/01_pacbio_qc_trim_assembly.sh
+++ code/script/01_pacbio_qc_trim_assembly.sh
@@ -1,8 +1,22 @@
 #!/bin/bash -l
 #SBATCH -A uppmax2026-1-61
 #SBATCH -p pelle
-#SBATCH -n 2
-#SBATCH -t 03:00:00
-#SBATCH -J pacbio_qc_and_preprocess
+#SBATCH -c 2
+#SBATCH -t 08:00:00
+#SBATCH -J pacbio_assembly
 #SBATCH --mail-type=ALL
 #SBATCH --output=%x.%j.out
+
+module load Canu
+module list
+
+cd /home/dinghy/Genome_Analysis_Project/data/genomics/pacbio/raw
+
+canu -p efm -d /home/dinghy/Genome_Analysis_Project/analysis/output/genomics/pacbio/canu_assembly_output \
+  genomeSize=3.0m \
+  useGrid=false \
+  maxThreads=2 \
+  corThreads=2 \
+  oeaThreads=2 \
+  cnsThreads=2 \
+  -pacbio-raw *.subreads.fastq.gz
